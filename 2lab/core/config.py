991bd5e2c5851c3af917346a0a19657d4ec5a926
VPKST <104562317+VPKST@users.noreply.github.com>
2025-04-14 13:28:51 +0300

    Delete 2lab/core directory

--- 2lab/core/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite+aiosqlite:///./app/db/database.db"
-    JWT_SECRET_KEY: str = "secretkey"
-    ALGORITHM: str = "HS256"
-
-settings = Settings()
