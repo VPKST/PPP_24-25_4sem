e8bd8e6bb1df4570e58a32cbbeac3b616ef4812d
VPKST <104562317+VPKST@users.noreply.github.com>
2025-04-14 13:33:17 +0300

    Create user.py

---
+++ 2lab/app/schemas/user.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    token: str
+
+class UserMe(BaseModel):
+    id: int
+    email: EmailStr
